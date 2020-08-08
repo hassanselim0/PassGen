6b0ee4bae57ac472236d6634c6b1c62c47ff3366
hassanselim0 <hassanselim0@hotmail.com>
2020-08-08 21:32:55 +0200

    Initial Commit

---
+++ PassGen-WPF/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PassGen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string StartingList { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (e.Args.Length == 1)
+            {
+                StartingList = e.Args[0];
+            }
+
+            if (e.Args.Length == 2)
+            {
+                PassGen.MainWindow.GetPass(e.Args[0], e.Args[1]);
+                Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ PassGen-WPF/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace PassGen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string keyList;
+        private string keyListPath => $"{keyList}.keys";
+        private List<string> keys;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var keyLists = Directory.EnumerateFiles(".", "*.keys")
+                .Select(f => Path.GetFileNameWithoutExtension(f)).ToList();
+
+            KeyListsCombo.ItemsSource = keyLists;
+            KeyListsCombo.Text = App.StartingList ?? "Default";
+
+            MasterBox.Focus();
+        }
+
+        private void KeyListsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            keyList = (sender as ComboBox)?.SelectedValue as string;
+
+            loadList();
+        }
+
+        private void KeyListsCombo_KeyUp(object sender, KeyEventArgs e)
+        {
+            keyList = (sender as ComboBox)?.Text as string;
+
+            loadList();
+        }
+
+        private void loadList()
+        {
+            keys = File.Exists(keyListPath) ?
+                File.ReadAllLines(keyListPath).ToList() : new List<string>();
+
+            KeysCombo.ItemsSource = keys.ElementAtOrDefault(1) == "" ?
+                keys.Skip(2) : keys;
+        }
+
+        private void GenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var check = GetPass(MasterBox.Password, "CHECK", false);
+
+            if (keys.Count < 2 || keys[1] != "")
+                keys.InsertRange(0, new[] { check, "" });
+            else if (keys[0] == "")
+                keys[0] = check;
+            else if (check != keys[0])
+                if (MessageBox.Show("Master Password check missmatch!\r\nGenerate anyways?",
+                    "Error", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+                    return;
+
+            var key = KeysCombo.Text;
+
+            if (!keys.Contains(key))
+                keys.Add(key);
+
+            File.WriteAllLines(keyListPath, keys);
+
+            GetPass(MasterBox.Password, key);
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public static string GetPass(string master, string key, bool clipboard = true)
+        {
+            var utf8 = Encoding.UTF8;
+            var hmac = new HMACSHA256(utf8.GetBytes(master));
+            var bytes = hmac.ComputeHash(utf8.GetBytes(key));
+            var pass = Convert.ToBase64String(bytes);
+
+            if (clipboard)
+            {
+                Clipboard.SetText(pass);
+                MessageBox.Show("Copied to Clipboard!", "Done!");
+            }
+
+            return pass;
+        }
+    }
+}
