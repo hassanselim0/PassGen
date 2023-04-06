7569d6df77f52bf7fa1199ac6adcebcecb58b2e5
hassanselim0 <hassanselim0@hotmail.com>
2023-04-06 02:53:17 +0200

    chore: bumped to version 1.2

--- passgen-py/setup.py
+++ passgen-py/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='passgen-py',
     packages=find_packages(),
-    version='1.1',
+    version='1.2',
     description='Generate Passwords Deterministically based on a Master Password.',
     classifiers=[
         'Development Status :: 3 - Alpha',
